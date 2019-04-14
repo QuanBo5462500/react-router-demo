898cbaa508c827f26fb100ce12a30594731dbd2c
全博 <bo.quan@hand-china.com>
2019-04-14 20:10:48 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Router, Route, Link, Switch } from 'react-router-dom';
+import createBrowserHistory from "history/createBrowserHistory";
 import './App.css';
 
-class App extends Component {
+
+import List1 from './Content/List1/index';
+import List2 from './Content/List2/index';
+import List3 from './Content/List3/index';
+import List4 from './Content/List4/index';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.showOther = this.showOther.bind(this);
+  }
+
+  showOther() {
+    return <div> render other </div>
+  }
+
   render() {
+    const history = createBrowserHistory();
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Router history={history}>
+          <>
+          <div className="left">
+              <ul>
+                <li><Link to="/">第一个List</Link></li>
+                <li><Link to="/List2">第二个List</Link></li>
+                <li><Link to="/List3">第三个List</Link></li>
+                <li><Link to="/List4">第四个List</Link></li>
+              </ul>
+            </div>
+            <div className="right">
+            <Switch>
+                <Route path="/" exact component={List1} />
+                <Route path="/List2" component={List2} />
+                <Route path="/List3" component={List3} />
+                <Route path="/List4" component={List4} />
+            </Switch>
+            </div>
+          </>
+        </Router>
       </div>
     );
   }
 }
 
-export default App;
